6086de203ce50c22993b3ee2b005d30e9d6ded9a
Rafael Sousa <rafael_david.2@hotmail.com>
2023-03-13 23:00:16 -0300

    Criando operaçao de instanciação

--- src/main/java/com/example/cursomc2/Cursomc2Application.java
+++ src/main/java/com/example/cursomc2/Cursomc2Application.java
@@ -1,13 +1,32 @@
 package com.example.cursomc2;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.cursomc2.domain.Categoria;
+import com.example.cursomc2.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class Cursomc2Application {
+public class Cursomc2Application implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cursomc2Application.class, args);
+	}
+
+	/** metodo para salvar uma categoria**/
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
